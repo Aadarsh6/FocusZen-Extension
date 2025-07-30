289a124ab63a2736e87eedc3de44b58c326e04a6
Adarsh <140302686+Aadarsh6@users.noreply.github.com>
2025-07-30 10:00:00 +0530

    visit website btn

--- popup.js
+++ popup.js
@@ -1,27 +1,3 @@
-// document.getElementById("toggleFocus").addEventListener("click", () => {
-//     chrome.storage.local.get("focusMode", (data) => {
-//       const newFocusMode = !data.focusMode;
-//       chrome.storage.local.set({ focusMode: newFocusMode }, () => {
-//         const statusText = newFocusMode ? "Focus Mode is ON" : "Focus Mode is OFF";
-//         const statusClass = newFocusMode ? "status-on" : "status-off";
-//         document.getElementById("focusStatus").textContent = statusText;
-//         document.getElementById("focusStatus").className = statusClass;
-//       });
-//     });
-//   });
-  
-//   // On load, display the current Focus Mode status
-//   chrome.storage.local.get("focusMode", (data) => {
-//     const statusText = data.focusMode ? "Focus Mode is ON" : "Focus Mode is OFF";
-//     const statusClass = data.focusMode ? "status-on" : "status-off";
-//     document.getElementById("focusStatus").textContent = statusText;
-//     document.getElementById("focusStatus").className = statusClass;
-//   });
-  
-
-
-// Function to update UI based on focus mode status
-// Function to update UI based on focus mode status
 function updateUI(isFocusModeOn) {
   const statusText = isFocusModeOn ? "Focus Mode is ON" : "Focus Mode is OFF";
   const statusClass = isFocusModeOn ? "status-on" : "status-off";
